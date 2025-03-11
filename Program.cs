ca04d5a4dce884193ffa1798c5c8785d211d1202
Erdem Carus <erdemcarus2@icloud.com>
2025-03-11 13:42:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Birinci sayıyı giriniz: ");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("İkinci sayıyı giriniz: ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Üçüncü sayıyı giriniz: ");
+int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Dördüncü sayıyı giriniz: ");
+int sayi4 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Beşinci sayıyı giriniz: ");
+int sayi5 = Convert.ToInt32(Console.ReadLine());
+
+int[] sayilar = { sayi1, sayi2, sayi3, sayi4, sayi5 };
+
+int toplam = 0;
+
+
+
+for (int i = 0; i < sayilar.Length; i++)
+{
+    toplam += sayilar[i];
+
+
+}
+
+double ortalama = (double)toplam / 5;
+
+Console.WriteLine(ortalama);
